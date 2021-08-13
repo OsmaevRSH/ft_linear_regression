dc997dbe486d19b6d74de9ebde496f113cb92233
ltheresi <osmaev.ruslan.bmstu@gmail.com>
2021-08-13 13:13:00 +0300

    update

--- linear_regression.py
+++ linear_regression.py
@@ -5,26 +5,28 @@ import numpy as np
 class LinearRegression:
     k_0 = 0.0
     k_1 = 0.0
-    # __accuracy = 0.0000000001
 
-    def __init__(self, data_for_predict: np.ndarray, verification_data: np.ndarray, learning_rate=0.1):
+    def __init__(self, data_for_predict: np.ndarray, verification_data: np.ndarray, learning_rate=0.01):
         self.__data_for_predict = data_for_predict
         self.__verification_data = verification_data
         self.__training_set_size = verification_data.size
         self.__learning_rate = learning_rate
         self.__z_normalize_data()
 
-    def __z_mse(self, data: np.ndarray):
+    @staticmethod
+    def __dispersion(data: np.ndarray):
         all_errors = (data - data.mean())
         all_errors = np.power(all_errors, 2)
-        return np.sum(all_errors) / (self.__training_set_size - 1)
+        return all_errors.mean()
 
     def __z_normalize_data(self):
-        dispersion = math.sqrt(self.__z_mse(self.__data_for_predict))
+        # Нормализация __data_for_predict
+        dispersion = math.sqrt(self.__dispersion(self.__data_for_predict))
         self.__data_for_predict = (self.__data_for_predict - self.__data_for_predict.mean())
         self.__data_for_predict /= dispersion
 
-        dispersion = math.sqrt(self.__z_mse(self.__verification_data))
+        # Нормализация __verification_data
+        dispersion = math.sqrt(self.__dispersion(self.__verification_data))
         self.__verification_data = (self.__verification_data - self.__verification_data.mean())
         self.__verification_data /= dispersion
 
@@ -44,19 +46,19 @@ class LinearRegression:
     def __calculation_k_0(self):
         all_errors = self.__error(self.__data_for_predict, self.__verification_data)
         middle_error = all_errors.mean()
-        return self.__learning_rate * middle_error
+        return middle_error
 
     def __calculation_k_1(self):
         all_errors = self.__error(self.__data_for_predict, self.__verification_data)
         all_errors *= self.__data_for_predict
         middle_error = all_errors.mean()
-        return self.__learning_rate * middle_error
+        return middle_error
 
     def training_model(self):
         # while self.__mse() > self.__accuracy:
-        for i in range(200000):
-            tmp_k_0 = self.k_0 - self.__calculation_k_0()
-            tmp_k_1 = self.k_1 - self.__calculation_k_1()
+        for i in range(20000):
+            tmp_k_0 = self.k_0 - self.__learning_rate * self.__calculation_k_0()
+            tmp_k_1 = self.k_1 - self.__learning_rate * self.__calculation_k_1()
             self.k_0 = tmp_k_0
             self.k_1 = tmp_k_1
             print('k0={}, k1={}, mse={}'.format(self.k_0, self.k_1, self.__mse()))
